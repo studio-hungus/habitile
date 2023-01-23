a4c23a56fc995bf45e7f7d028b11ba53c8b3b6db
Jeffrey Harmon <jeffrey.harmon@bsu.edu>
2023-01-23 11:50:27 -0500

    Add snap to board

---
+++ project/Board/Board.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+export var width := 1
+export var height := 1
+
+func _ready():
+	pass
--- project/Tile/Tile.gd
+++ project/Tile/Tile.gd
@@ -1,17 +1,34 @@
 extends Node2D
 
+onready var area := find_node("Area2D")
+
 var _held := false
 
+
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
-	if event is InputEventMouseButton and event.is_pressed():
-		_held = true
+	if event is InputEventMouseButton:
+		_held = event.is_pressed()
 		print("Hello or something")
-	if event is InputEventMouseButton and not event.is_pressed():
-		_held = false
-		print("I am unpressed")
+		if not event.is_pressed():
+			_drop()
+
 
 func _input(event):
 	if _held and event is InputEventMouseMotion:
 		global_position = get_global_mouse_position()
 	
+func _drop():
+	var emptySpace : Area2D
+	var distance := -1.0
 	
+	if area.get_overlapping_areas().empty():
+		return
+	
+	for overlap in area.get_overlapping_areas():
+		if overlap.is_in_group("EmptySpace"):
+			var current_distance = overlap.global_position.distance_to(global_position)
+			if distance < 0 or distance > current_distance:
+				emptySpace = overlap
+				distance = current_distance
+				emptySpace = overlap
+	global_position = emptySpace.global_position
