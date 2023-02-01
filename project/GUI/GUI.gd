0ffa08c948ec8796021e2fe0237af1cbda33bf01
Jeffrey Harmon <jeffrey.harmon@bsu.edu>
2023-02-01 10:25:54 -0500

    Add turn indication to GUI

--- project/GUI/GUI.gd
+++ project/GUI/GUI.gd
@@ -4,12 +4,20 @@ extends Control
 onready var left_panel = $CanvasLayer/LeftPanel
 onready var right_panel = $CanvasLayer/RightPanel
 
+var style_active = StyleBoxFlat.new()
+var style_inactive = StyleBoxFlat.new()
+
+func _ready():
+	style_active.set_bg_color(Color("#ADD8E6"))
 
 func set_turn(is_player_one_turn):
 	if is_player_one_turn:
 		left_panel.mouse_filter = Control.MOUSE_FILTER_IGNORE
 		right_panel.mouse_filter = Control.MOUSE_FILTER_STOP
-
+		left_panel.add_stylebox_override("panel", style_active)
+		right_panel.add_stylebox_override("panel", style_inactive)
 	else:
 		left_panel.mouse_filter = Control.MOUSE_FILTER_STOP
 		right_panel.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		left_panel.add_stylebox_override("panel", style_inactive)
+		right_panel.add_stylebox_override("panel", style_active)
