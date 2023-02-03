b391d0b2f5d1d1fbe3b02707ae223f5eba0a47f6
Granger <cagranger@bsu.edu>
2023-02-03 09:58:08 -0500

    Indicate variable privacy

--- project/Game/Game.gd
+++ project/Game/Game.gd
@@ -1,20 +1,18 @@
 extends Node2D
 
 
-export var held_tile_z_index := 16
-var original_tile_z_index := 0
-var pressed_tile : Node2D
-var hovered_node : Node2D
-var original_tile_position := Vector2(0, 0)
-var is_player_one_turn := true
-var stack_left := []
-var stack_right := []
+export var _held_tile_z_index := 16
+var _original_tile_z_index := 0
+var _pressed_tile : Node2D
+var _hovered_node : Node2D
+var _original_tile_position := Vector2(0, 0)
+var _is_player_one_turn := true
 
-onready var tiles := find_node("Tiles")
-onready var board := find_node("Board")
-onready var no_drop_sound := find_node("NoDropSound")
-onready var drop_sound := find_node("DropSound")
-onready var gui := find_node("GUI")
+onready var _tiles := find_node("Tiles")
+onready var _board := find_node("Board")
+onready var _no_drop_sound := find_node("NoDropSound")
+onready var _drop_sound := find_node("DropSound")
+onready var _gui := find_node("GUI")
 
 
 func _ready():
@@ -22,12 +20,12 @@ func _ready():
 	for i in 6:
 		_make_new_tile()
 
-	tiles.get_child(0).global_position = $LeftPlayerPosition1.global_position
-	tiles.get_child(1).global_position = $LeftPlayerPosition2.global_position
-	tiles.get_child(2).global_position = $LeftPlayerPosition3.global_position
-	tiles.get_child(3).global_position = $RightPlayerPosition1.global_position
-	tiles.get_child(4).global_position = $RightPlayerPosition2.global_position
-	tiles.get_child(5).global_position = $RightPlayerPosition3.global_position
+	_tiles.get_child(0).global_position = $LeftPlayerPosition1.global_position
+	_tiles.get_child(1).global_position = $LeftPlayerPosition2.global_position
+	_tiles.get_child(2).global_position = $LeftPlayerPosition3.global_position
+	_tiles.get_child(3).global_position = $RightPlayerPosition1.global_position
+	_tiles.get_child(4).global_position = $RightPlayerPosition2.global_position
+	_tiles.get_child(5).global_position = $RightPlayerPosition3.global_position
 
 	_set_turn()
 
@@ -36,45 +34,45 @@ func _physics_process(_delta):
 	var current_mouse_position = get_global_mouse_position()
 	var found_hover = false
 
-	if pressed_tile != null:
-		pressed_tile.global_position = current_mouse_position
+	if _pressed_tile != null:
+		_pressed_tile.global_position = current_mouse_position
 
 	# empty_space for loop needs to be first 
-	for empty_space in board.get_empty_spaces():
+	for empty_space in _board.get_empty_spaces():
 		if empty_space.contains(current_mouse_position):
-			hovered_node = empty_space
+			_hovered_node = empty_space
 			found_hover = true
 
-	for tile in tiles.get_children():
-		if tile != pressed_tile and tile.contains(current_mouse_position):
-			hovered_node = tile
+	for tile in _tiles.get_children():
+		if tile != _pressed_tile and tile.contains(current_mouse_position):
+			_hovered_node = tile
 			found_hover = true
 			
 	if not found_hover:
-		hovered_node = null
+		_hovered_node = null
 
 
 func _on_Tile_pressed(tile):
-	pressed_tile = tile
-	original_tile_position = tile.global_position
+	_pressed_tile = tile
+	_original_tile_position = tile.global_position
 
-	original_tile_z_index = tile.z_index
-	tile.z_index = held_tile_z_index
+	_original_tile_z_index = tile.z_index
+	tile.z_index = _held_tile_z_index
 
 	tile.connect("released", self, "_on_Tile_released", [tile])
 
 
 func _on_Tile_released(tile):
-	pressed_tile = null
-	tile.z_index = original_tile_z_index
+	_pressed_tile = null
+	tile.z_index = _original_tile_z_index
 
-	if hovered_node is Tile:
-		no_drop_sound.play()
-		tile.global_position = original_tile_position
+	if _hovered_node is Tile:
+		_no_drop_sound.play()
+		tile.global_position = _original_tile_position
 
-	elif hovered_node is EmptySpace:
-		drop_sound.play()
-		tile.global_position = hovered_node.global_position
+	elif _hovered_node is EmptySpace:
+		_drop_sound.play()
+		tile.global_position = _hovered_node.global_position
 
 		_swap_turn()
 		tile.set_placed()
@@ -82,25 +80,25 @@ func _on_Tile_released(tile):
 		_make_new_tile()
 
 	else:
-		tile.global_position = original_tile_position
+		tile.global_position = _original_tile_position
 	
 	tile.disconnect("released", self, "_on_Tile_released")
 
 
 func _set_turn():
-	gui.set_turn(is_player_one_turn)
+	_gui.set_turn(_is_player_one_turn)
 
 
 func _swap_turn():
-	is_player_one_turn = !is_player_one_turn
+	_is_player_one_turn = !_is_player_one_turn
 	_set_turn()
 	
 
 # This creates a tile in the original tile position
 func _make_new_tile() -> void:
 	var tile = preload("res://Tile/Tile.tscn").instance()
-	tiles.add_child(tile)
-	tile.global_position = original_tile_position
+	_tiles.add_child(tile)
+	tile.global_position = _original_tile_position
 	tile.set_type(tile.TYPE.values()[randi() % tile.TYPE.size()])
 	
 	tile.connect("pressed", self, "_on_Tile_pressed", [tile])
