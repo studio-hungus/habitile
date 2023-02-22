d7b994cef7a7241c1b4f473e6f142687ec0b04b5
Dotson <lwdotson@bsu.edu>
2023-02-22 11:12:32 -0500

    Add endgame button textures

--- project/EndGameGUI/EndGameGUI.gd
+++ project/EndGameGUI/EndGameGUI.gd
@@ -2,9 +2,6 @@ extends Control
 
 
 onready var _win_state_text = find_node("WinState")
-onready var _win_tree = find_node("WinTree")
-onready var _left_player_position = find_node("LeftPlayer")
-onready var _right_player_position = find_node("RightPlayer")
 onready var _draw_sound = find_node("DrawSound")
 onready var _win_sound = find_node("WinSound")
 onready var _animation_player = find_node("AnimationPlayer")
@@ -22,21 +19,19 @@ func display_gameover_screen(end_state):
 
 func _display_draw():
 	_win_gradient.visible = false
-	_win_state_text.text = ("DRAW")
+	_win_state_text.bbcode_text = "[center]DRAW[/center]"
 	_draw_sound.play()
 
 
 func _display_win(end_state):
 	_win_gradient.visible = true
 	if end_state == Game.EndState.LEFT_WIN:
-		_win_state_text.text = ("Left Player Wins!")
+		_win_state_text.bbcode_text = "[center]Left Player Wins![/center]"
 		_animation_player.play("LeftWin")
 		_win_gradient.rect_scale = Vector2(1,1)
 	elif end_state == Game.EndState.RIGHT_WIN:
 		_win_gradient.rect_scale = Vector2(-1,1)
-		_win_state_text.text = ("Right Player Wins!")
-		_win_tree.global_position = _right_player_position.global_position
-		_animation_player.play("RightWin")
+		_win_state_text.bbcode_text = "[center]Right Player Wins![/center]"
 		_animation_player.play("RightWin")
 	_win_sound.play()
 
