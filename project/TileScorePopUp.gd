8bab2cf889223bfef2da9386bb955bb2d38e6f09
Brooke Wright <brooke.wright@bsu.edu>
2023-03-15 09:24:03 -0400

    Remove testing prototypes for a card redesign

---
+++ project/TileScorePopUp.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+func init(positive: bool, pointAmount : int) -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
