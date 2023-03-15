8089e21e26d101d9b0c649390f9589031cdc5d40
Brooke Wright <brooke.wright@bsu.edu>
2023-03-15 09:56:55 -0400

    Center and increase size for score indication text

--- project/Tile/Tile.gd
+++ project/Tile/Tile.gd
@@ -27,20 +27,17 @@ onready var _icons := get_node("Icons")
 func contains(mouse_position:Vector2) -> bool:
 	var center := global_position - size / 2
 	var rect = Rect2(center, size)
-
 	return rect.has_point(mouse_position)
 
 
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
-	if _interactable:
-		if event is InputEventMouseButton:
-			if event.is_pressed():
-				_pressed = true
-				emit_signal("pressed")
-
-			else:
-				_pressed = false
-				emit_signal("released")
+	if _interactable and event is InputEventMouseButton:
+		if event.is_pressed():
+			_pressed = true
+			emit_signal("pressed")
+		else:
+			_pressed = false
+			emit_signal("released")
 
 
 func _process(_delta):
@@ -61,6 +58,7 @@ func initialize_type(init_type: TileType):
 	_supply_sprite.texture = type.in_supply_texture
 	_board_sprite.texture = type.on_board_texture
 	_name_label.text = type.name
+
 	get_node("AnimalName/Plus").text = "+%s" % type.positive_score_modifier
 	get_node("AnimalName/Minus").text = "-%s" % abs(type.negative_score_modifier)
 	for i in len(type.postive_icons_textures):
@@ -113,7 +111,6 @@ func calculate_points(neighbors: Array) -> int:
 	var points := 0
 	var delay_time = 0
 	var delay_increment = .15
-	
 
 	for neighbor in neighbors:
 
@@ -122,7 +119,6 @@ func calculate_points(neighbors: Array) -> int:
 			_show_score_modified(type.negative_score_modifier, neighbor.find_node("ScoreEarned"), delay_time)
 			#play bad noise here
 			delay_time += delay_increment
-			continue
 
 		if type.positive_neighbor_tiles.has(neighbor.type):
 			_show_score_modified(type.positive_score_modifier, neighbor.find_node("ScoreEarned"), delay_time)
@@ -134,5 +130,9 @@ func calculate_points(neighbors: Array) -> int:
 			points += type.negative_score_modifier
 			#play bad noise here
 			delay_time += delay_increment
+		else:
+			_show_score_modified(0, neighbor.find_node("ScoreEarned"), delay_time)
+			delay_time += delay_increment
+			continue
 
 	return points
