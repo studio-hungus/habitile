a42104ed201f606547245c7b4539dc9d62446853
PandoraGit <waroberts@bsu.edu>
2023-03-17 09:44:04 -0400

    Refactor audio end music to animation player

--- project/Soundtrack/AudioManager.gd
+++ project/Soundtrack/AudioManager.gd
@@ -9,17 +9,4 @@ func play_main_theme():
 
 
 func play_gameover():
-	_main_theme.stop()
-	_end_theme.play()
-
-
-func _increase_volume(audio: AudioStreamPlayer, volume_db ):
-	if ( audio.volume_db != volume_db ):
-		var decibels = .5
-		audio.set_volume_db( audio.volume_db + decibels )
-
-
-func _process(_delta):
-#for every frame ...
-	if _end_theme.is_playing():
-		_increase_volume( _end_theme , 0 )
+	$AnimationPlayer.play("GameOver")
