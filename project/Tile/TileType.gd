26e935e77d53dfb88cd1ca514626210776ba5da3
Miller <jadi.miller@bsu.edu>
2023-03-20 10:02:19 -0400

    Add string arrays for neighbors

--- project/Tile/TileType.gd
+++ project/Tile/TileType.gd
@@ -4,9 +4,8 @@ extends Resource
 export(String) var name := ""
 export(int) var positive_score_modifier := 0
 export(int) var negative_score_modifier := 0
-export(Array, Resource) var positive_neighbor_tiles := []
-export(bool) var allergic_to_grass := false
+export(Array, String) var positive_neighbor_names := []
+export(Array, String) var negative_neighbor_names := []
 export(Texture) var in_supply_texture
 export(Texture) var on_board_texture
-export(Array, Texture) var postive_icons_textures
-export(Array, Texture) var negative_icons_textures
+
