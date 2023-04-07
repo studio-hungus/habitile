cbdc6c5ce777b3375e9168f12eafabf2b096e152
Lyle Dotson <ld77668952@gmail.com>
2023-04-06 21:43:53 -0400

    Fix mute button defect

--- project/SettingsBar/SettingsBar.gd
+++ project/SettingsBar/SettingsBar.gd
@@ -1,11 +1,16 @@
 extends Control
 
 onready var fullscreen_button := find_node("FullscreenButton")
+onready var mute_button := find_node("MuteButton")
+
 
 
 func _ready():
 	if OS.window_fullscreen:
 		fullscreen_button.pressed = true
+
+	if AudioServer.is_bus_mute(0):
+		mute_button.pressed = true
 
 
 func _on_FullscreenButton_pressed():
