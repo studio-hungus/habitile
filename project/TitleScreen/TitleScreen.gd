fb92b251b117361d77be6b532b480aacb49fa3e2
Granger <cagranger@bsu.edu>
2023-04-12 11:38:41 -0400

    Add textures to quit button

--- project/TitleScreen/TitleScreen.gd
+++ project/TitleScreen/TitleScreen.gd
@@ -1,6 +1,22 @@
 extends Control
 
 
+onready var _fullscreen_button := find_node("FullscreenButton")
+onready var _mute_button := find_node("MuteButton")
+onready var _resign_dialog := find_node("ResignDialogLayer")
+
+
+func _ready():
+	
+	if OS.window_fullscreen:
+		_fullscreen_button.pressed = true
+
+	if AudioServer.is_bus_mute(0):
+		_mute_button.pressed = true
+
+
+
+
 func _on_Play_pressed():
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Game/Game.tscn")
