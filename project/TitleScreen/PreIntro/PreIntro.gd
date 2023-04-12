6c670757987a328be09e0dcc8c838deb7ecde45b
Granger <cagranger@bsu.edu>
2023-04-12 11:32:07 -0400

    Add title screen music

--- project/TitleScreen/PreIntro/PreIntro.gd
+++ project/TitleScreen/PreIntro/PreIntro.gd
@@ -1,14 +1,19 @@
 extends Control
 
 
+onready var _animation_player := find_node("AnimationPlayer")
+onready var _fade := find_node("Fade")
+
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.pressed:
 			if event.button_index == 1:
-				$AnimationPlayer.play("Fade")
-				
+				_animation_player.play("Fade")
+
+
 func invisible():
-	$ColorRect.visible = false
+	_fade.visible = false
 
 
 func _on_AnimationPlayer_animation_finished(_anim_name):
