06d64e429d429a429396cd475ddca537638f3e80
Brooke Wright <brooke.wright@bsu.edu>
2023-04-26 08:51:12 -0400

    Update script to follow style guide

--- project/Soundtrack/AudioManager.gd
+++ project/Soundtrack/AudioManager.gd
@@ -5,15 +5,15 @@ onready var _game_over_stinger_theme := find_node("GameOverStinger")
 onready var _animation_player := find_node("AnimationPlayer")
 
 
-func play_main_theme():
+func play_main_theme() -> void:
 	_main_theme.play()
 
 
-func play_gameover_tie():
+func play_gameover_tie() -> void:
 	_game_over_stinger_theme.stream = load("res://Soundtrack/GL_Stinger_Draw.wav")
 	_animation_player.play("GameOver")
 
 
-func play_gameover_win():
+func play_gameover_win() -> void:
 	_game_over_stinger_theme.stream = load("res://Soundtrack/GL_Sting_Win.wav")
 	_animation_player.play("GameOver")
